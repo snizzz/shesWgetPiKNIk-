1c9b5b827cdd73525f724f64f17ed8a3190bf7f8
Luminatione <41484837+Luminatione@users.noreply.github.com>
2021-10-15 19:51:47 +0200

    Zadanie Cats

---
+++ Cats/Cats/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace Cats
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(91, 265);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(168, 32);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Następna Ciekawostka";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.label2.Location = new System.Drawing.Point(87, 7);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(172, 24);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Twoja ciekawostka:";
+			this.label2.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(12, 56);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.ReadOnly = true;
+			this.richTextBox1.Size = new System.Drawing.Size(324, 190);
+			this.richTextBox1.TabIndex = 3;
+			this.richTextBox1.Text = "";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(346, 311);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Koty";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+	}
+}
+
---
+++ Cats/Cats/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace Cats
+{
+	public partial class Form1 : Form
+	{
+
+		private String api = "https://catfact.ninja/fact";
+		private string currentFact;
+		private Thread updateThread;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			LoadCatFact();
+		}
+
+		string GetFactFromJson(JObject json) => json["fact"].ToString();
+
+		private void SetLabelTextToCurrentCat()
+		{
+			richTextBox1.Text = currentFact;
+			GC.Collect();
+		}
+		private void GetFact()
+		{
+			WebClient client = new WebClient();
+			currentFact = GetFactFromJson(JObject.Parse(client.DownloadString(api)));
+			richTextBox1.Invoke((MethodInvoker)SetLabelTextToCurrentCat);
+		}
+
+		private void LoadCatFact()
+		{
+			updateThread = new Thread(GetFact);
+			updateThread.Start();
+		}
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			LoadCatFact();
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
