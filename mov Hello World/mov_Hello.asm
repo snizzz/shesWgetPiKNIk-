111062c1963fd0637bf4b0a2a723fb517507e513
snizzz <45829927+snizzz@users.noreply.github.com>
2021-10-17 17:32:33 +0200

    Update mov_Hello.asm

--- mov Hello World/mov_Hello.asm
+++ mov Hello World/mov_Hello.asm
@@ -1,14 +1,14 @@
 global    _start
 
           section   .text
-_start:   mov       rax, 1                  ; system call for write
-          mov       rdi, 1                  ; file handle 1 is stdout
-          mov       rsi, message            ; address of string to output
-          mov       rdx, 13                 ; number of bytes
-          syscall                           ; invoke operating system to do the write
-          mov       rax, 60                 ; system call for exit
-          xor       rdi, rdi                ; exit code 0
-          syscall                           ; invoke operating system to exit
+_start:   mov       rax, 1                  
+          mov       rdi, 1                  
+          mov       rsi, message          
+          mov       rdx, 13                 
+          syscall                        
+          mov       rax, 60                 
+          xor       rdi, rdi                
+          syscall                           
 
           section   .data
 message:  db        "HACKATHON", 10
